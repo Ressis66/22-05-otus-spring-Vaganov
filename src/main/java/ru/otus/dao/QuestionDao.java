1b022d4203a8ea332de602e118afd8bc0288102c
Ressis66 <vaganov.aleksandr.88@mail.ru>
2022-06-09 07:58:07 +0500

    First commit

---
+++ src/main/java/ru/otus/dao/QuestionDao.java
@@ -0,0 +1,13 @@
+package ru.otus.dao;
+
+
+import ru.otus.domain.Question;
+
+
+import java.util.List;
+
+public interface QuestionDao {
+  List<Question> getList();
+  Question getQuestion(int id);
+
+}
